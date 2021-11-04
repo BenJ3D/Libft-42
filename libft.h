9d783772f0a5dffafa40c6ad45eb5f33db5f3696
Benjamin Ducrocq <bducrocq@z1r11p3.42lyon.fr>
2021-11-04 10:32:12 +0100

    save github

--- libft.h
+++ libft.h
@@ -6,16 +6,23 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 14:59:56 by bducrocq          #+#    #+#             */
-/*   Updated: 2021/11/02 16:31:42 by bducrocq         ###   ########.fr       */
+/*   Updated: 2021/11/04 10:31:28 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-unsigned int	ft_strlen(char *str);
+#include <string.h>
+size_t	ft_strlen(char *str);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
+void *ft_memset	(void *b, int c, size_t len);
+
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-libft main :
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -8,13 +6,14 @@ libft main :
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:51:43 by bducrocq          #+#    #+#             */
-/*   Updated: 2021/11/03 13:32:27 by bducrocq         ###   ########.fr       */
+/*   Updated: 2021/11/04 10:28:48 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <ctype.h>
 #include "libft.h"
+#include <ctype.h>
+#include <string.h>
 
 void red ()
 {
@@ -69,11 +68,60 @@ int main()
 	printf ("%d", isalnum(alnum[3]));
 	printf ("%d\n", isalnum(alnum[4]));
 	reset ();
-	// printf ("%d", ft_isalnum(alnum[0]));
-	// printf ("%d", ft_isalnum(alnum[1]));
-	// printf ("%d", ft_isalnum(alnum[2]));
-	// printf ("%d", ft_isalnum(alnum[3]));
-	// printf ("%d\n", ft_isalnum(alnum[4]));
+	printf ("%d", ft_isalnum(alnum[0]));
+	printf ("%d", ft_isalnum(alnum[1]));
+	printf ("%d", ft_isalnum(alnum[2]));
+	printf ("%d", ft_isalnum(alnum[3]));
+	printf ("%d\n", ft_isalnum(alnum[4]));
+	
+	purple ();	
+	printf("------------------\n");
+	red ();
+	printf("ISASCII\n");
+	reset ();
+	int isas [] = {-1, 0, 42, 110, 255};
+	green ();
+	printf("%d", isascii(isas[0]));
+	printf("%d", isascii(isas[1]));
+	printf("%d", isascii(isas[2]));
+	printf("%d", isascii(isas[3]));
+	printf("%d\n", isascii(isas[4]));
+	reset();
+	printf("%d", ft_isascii(isas[0]));
+	printf("%d", ft_isascii(isas[1]));
+	printf("%d", ft_isascii(isas[2]));
+	printf("%d", ft_isascii(isas[3]));
+	printf("%d\n", ft_isascii(isas[4]));
+	
+	purple ();	
+	printf("------------------\n");
+	red ();
+	printf("ISPRINT\n");
+	reset ();
+	int isprt [] = {31, 32, 126, 217, -4};
+	green ();
+	printf("%d", isprint(isprt[0]));
+	printf("%d", isprint(isprt[1]));
+	printf("%d", isprint(isprt[2]));
+	printf("%d", isprint(isprt[3]));
+	printf("%d\n", isprint(isprt[4]));
+	reset();
+	printf("%d", ft_isprint(isprt[0]));
+	printf("%d", ft_isprint(isprt[1]));
+	printf("%d", ft_isprint(isprt[2]));
+	printf("%d", ft_isprint(isprt[3]));
+	printf("%d\n", ft_isprint(isprt[4]));
+	
+	purple ();	
+	printf("------------------\n");
+	red ();
+	printf("STRLEN\n");
+	reset ();
+	char stln [] = "Bonjour je fais 23 char";
+	green ();
+	printf("%lu\n", strlen(stln));
+	reset ();
+	printf("%lu\n", ft_strlen(stln));
 
 	purple ();	
 	printf("------------------\n");
@@ -83,7 +131,7 @@ int main()
 	char tab [] = "Coucou";
 	char tab2 [] = "Coucou";
 	printf("%s\n", tab);
-	//ft_memset(tab, 33, 3);
+	ft_memset(tab, 33, 3);
 	green ();
 	memset(tab2, 33, 3);
 	printf("=======>>>>>>> %s\n", tab2);
@@ -94,8 +142,21 @@ int main()
 	purple ();	
 	printf("------------------\n");
 	red ();
-	printf("XXX-XXX\n");
+	printf("STRLCPY\n");
 	reset ();
+	char lcpy [] = "coucou les zouzous et";
+	char lcpy2 [22];
+	char lcpy3 [22];
+
+	printf("%s\n", lcpy);
+	green ();
+	strlcpy(lcpy3, lcpy, 12);
+	printf("%s\n", lcpy3);
+	reset ();
+	ft_strlcpy(lcpy2, lcpy, 12);
+	printf("%s\n", lcpy2);
+
+
 }
 
 /* 
