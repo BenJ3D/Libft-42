ae655b863e19ea1232d182bf9aa6266b860a0096
Benjamin Ducrocq <bducrocq@z2r4p1.42lyon.fr>
2021-11-05 12:31:26 +0100

    save

---
+++ ft_strrchr copy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 22:53:31 by bducrocq          #+#    #+#             */
+/*   Updated: 2021/11/05 12:30:10 by bducrocq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char *ft_strrchr(const char *s, int c)
+{	
+	size_t i;
+	i = ft_strlen(s);
+	while (i > 0)
+	{
+		if (s[i] == c)
+			return ((char*)s+i);
+		i--;
+	}
+		return (0);
+}
+
