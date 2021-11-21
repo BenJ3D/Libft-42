34fbd6514a16ea3d13a72a814c8f452fd8512ec9
B3D STUDIO <49345674+BenJ3D@users.noreply.github.com>
2021-11-21 12:55:33 +0100

    Create while8test.c

---
+++ while8test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+ int    nb;
+
+ nb = 0;
+
+ while (nb < 6666)
+ {
+        nb++;
+        if (nb == 666)
+                nb = 0;
+
+        if ((nb % 2) == 0)
+                printf("**");
+        if ((nb % 2) == 1)
+                printf("//");
+ }
+
+}
